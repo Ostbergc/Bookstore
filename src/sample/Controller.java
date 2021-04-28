60adced269a45c91fcefc3a291d90e9927a5fdb1
besnikskola <57898556+besnikskola@users.noreply.github.com>
2021-04-28 05:50:37 +0200

    first commit, besnik & bujar login register

---
+++ src/sample/Controller.java
@@ -0,0 +1,77 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class Controller {
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private PasswordField pwTextField;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private Button logOutBtn;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private Label passwordLabel;
+
+    SQLConnector connector = new SQLConnector();
+
+    public void initReg() {
+
+        String pw = pwTextField.getText();
+        String email = emailTextField.getText();
+        Main.userOfApp = new User(email, pw, "testName", "testLastName", "testAddress", "testCity", "testZip", "testState", "testCountry", false);
+        connector.createUser(Main.userOfApp);
+        emailTextField.clear();
+        pwTextField.clear();
+
+    }
+
+    public void initLog() {
+
+        String pw = pwTextField.getText();
+        String email = emailTextField.getText();
+        Main.isLoggedIn = connector.verifyLogin(pw, email);
+        System.out.println("isLoggedIn: " + Main.isLoggedIn);
+        if (Main.isLoggedIn == true) {
+            logOutBtn.setVisible(true);
+            loginBtn.setVisible(false);
+            registerButton.setVisible(false);
+            emailLabel.setVisible(false);
+            passwordLabel.setVisible(false);
+            emailTextField.setVisible(false);
+            pwTextField.setVisible(false);
+        }
+        emailTextField.clear();
+        pwTextField.clear();
+    }
+
+    public void initLogOut() {
+        Main.isLoggedIn = false;
+        Main.userOfApp = null;
+        logOutBtn.setVisible(false);
+        registerButton.setVisible(true);
+        loginBtn.setVisible(true);
+        emailLabel.setVisible(true);
+        passwordLabel.setVisible(true);
+        emailTextField.setVisible(true);
+        pwTextField.setVisible(true);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Log Out");
+        alert.setContentText("You have successfully logged out.");
+        alert.show();
+    }
+
+}
---
+++ src/sample/SQLConnector.java
@@ -0,0 +1,130 @@
+package sample;
+
+
+import javafx.scene.control.Alert;
+
+import java.awt.print.Book;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class SQLConnector {
+
+    /*TODO WARNING***********************************************************************
+      TODO MUST GET SQL JAR OR SOMETHING TO CONNECT TO DATABASE INTO LIBRARY
+      TODO********************************************************************************************
+     */
+    private String url = "jdbc:mysql://den1.mysql6.gear.host:3306/group10bookstore?user=group10bookstore&password=Bi3o_!AcSP51&serverTimezone=UTC";
+
+    Connection connection;
+    Statement statement;
+    ResultSet resultSet;
+
+    //use this to connect to the database
+    public void connect() {
+        try {
+            System.out.println("Connection successfully established..");
+            connection = DriverManager.getConnection(url);
+
+        } catch (SQLException sq) {
+            sq.printStackTrace();
+            System.out.println(sq.getMessage());
+        }
+    }
+
+
+    public void createUser(User user) {
+        System.out.println("Entered createuser");
+
+        try {
+            connect();
+            String sql = "INSERT INTO `users` (`email`, `password`, `firstname`, `lastname`, `address`, `city`, `zip`, `state`, `country`, `isEmployee`) VALUES " +
+                    "('" + user.getEmail() + "', '" + user.getPassword() + "', '" + user.getFirstname() + "', '" + user.getLastname() + "', '" + user.getAddress() + "', " +
+                    "'" + user.getCity() + "', '" + user.getZip() + "', '" + user.getState() + "', '" + user.getCountry() + "', '0');";
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setContentText("User has been registered, you may now log in.");
+            alert.show();
+            System.out.println("User created: " + user.toString());
+
+        } catch (SQLIntegrityConstraintViolationException e) {
+            System.out.println("SQLIntegrityConstraintViolationException: email already exists in database. ");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setContentText("Email has already been taken. Please choose a different email for registration.");
+            alert.show();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public boolean verifyLogin(String userIn, String pwIn) {
+        System.out.println("Entered verifylogin.");
+        boolean isVerified = false;
+        try {
+            connect();
+            String sql = "SELECT email, password FROM users WHERE email = '" + userIn + "' AND password = '" + pwIn + "';";
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+
+
+            while (resultSet.next()) {
+                if (resultSet.getString("email").equals(userIn) && resultSet.getString("password").equals(pwIn)) {
+                    System.out.println(resultSet.getString("email") + " is email");
+                    System.out.println(resultSet.getString("password") + " is password");
+                    isVerified = true;
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Success");
+                    alert.setContentText("You have successfully logged in.");
+                    alert.show();
+                }
+            }
+
+            if (isVerified == false) {
+                System.out.println("Wrong email and/or password.");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setContentText("Wrong email and/or password.");
+                alert.show();
+            }
+            disconnect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return isVerified;
+    }
+
+
+    //disconnect from the database
+    public void disconnect() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+}
+
+
+
+
+
